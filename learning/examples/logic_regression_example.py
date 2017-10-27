9f8c772bc8184ed0295885057d8693eba6e6c986
JeffreyW2947 <wang@jwtherookie.com>
2017-10-27 16:01:56 -0400

    Add files via upload

---
+++ learning/examples/logic_regression_example.py
@@ -0,0 +1,13 @@
+from machinelearning.classify import LogicRegression
+
+demo = LogicRegression()
+demo.ReadSimpleFile("testSet2.txt")
+testSet, testLabel = demo.SeparateDataSet()
+
+demo.GradientAscent()
+
+#demo.StochasticGradientAscent()
+
+line = demo.GetLine()
+demo.Graph(line)
+demo.SmartTest(testSet, testLabel)
