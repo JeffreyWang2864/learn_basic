6094a6fe0843fa565122ffa9a270fcb485c353d0
JeffreyW2947 <wang@jwtherookie.com>
2017-11-06 11:51:40 -0500

    Update test.py

--- learning/test.py
+++ learning/test.py
@@ -2,42 +2,42 @@ import numpy as np
 
 from machinelearning.regression import Regression
 
-# demo = Regression()
-#
-# def loadDataSet(fileName):
-#     dataMat = []
-#     fr = open(fileName)
-#     x, y = list(), list()
-#     for line in fr.readlines():
-#         curLine = line.strip().split('\t')
-#         x.append(float(curLine[0]))
-#         y.append(float(curLine[1]))
-#     return np.array(np.mat(x).transpose()), np.array(y)
-#     #return np.array(x), np.array(y)
-#
-# a, b = loadDataSet('/Users/Excited/PycharmProjects/learning/DATA/归档/sine.txt')
-# demo.DataSet = a
-# demo.Labels = b
-# demo.Title = ['111', ]
-# testD, testL = demo.SeparateDataSet()
-#
-# newD, newL = list(), list()
-# aa = [float(item) for item in testD]
-# sortIndex = np.array(aa).argsort()
-# for item in sortIndex:
-#     newD.append(float(testD[item]))
-#     newL.append(float(testL[item]))
-# newD = np.array(newD)
-# newL = np.array(newL)
-# newD = np.array(np.mat(newD).transpose())
-#
-# demo.MIN_NUM_DATA = 5
-# demo.MIN_DELTA_ERR = 1.0
-#
-# demo.LinearRegression([0, ], mode=demo.TREE_BASED)
-# rs, predicted = demo.SmartTest(newD, newL, weight=0.4, toggle_print=True)
-# print(rs)
-# demo.Graph(0, ('x', 'y'), [newD, predicted])
+demo = Regression()
+
+def loadDataSet(fileName):
+    dataMat = []
+    fr = open(fileName)
+    x, y = list(), list()
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        x.append(float(curLine[0]))
+        y.append(float(curLine[1]))
+    return np.array(np.mat(x).transpose()), np.array(y)
+    #return np.array(x), np.array(y)
+
+a, b = loadDataSet('/Users/Excited/PycharmProjects/learning/DATA/归档/exp2.txt')
+demo.DataSet = a
+demo.Labels = b
+demo.Title = ['111', ]
+testD, testL = demo.SeparateDataSet()
+
+newD, newL = list(), list()
+aa = [float(item) for item in testD]
+sortIndex = np.array(aa).argsort()
+for item in sortIndex:
+    newD.append(float(testD[item]))
+    newL.append(float(testL[item]))
+newD = np.array(newD)
+newL = np.array(newL)
+newD = np.array(np.mat(newD).transpose())
+
+demo.MIN_NUM_DATA = 5
+demo.MIN_DELTA_ERR = 1.0
+
+demo.LinearRegression([0, ], mode=demo.TREE_BASED)
+rs, predicted = demo.SmartTest(newD, newL, weight=0.4, toggle_print=True)
+print(rs)
+demo.Graph(0, ('x', 'y'), [newD, predicted])
 
 
 # demo.ReadSimpleFile("pca_dataset.txt")
@@ -69,3 +69,11 @@ from machinelearning.regression import Regression
 # rs, predicted = demo.SmartTest(newD[:, [0, ]], newL)
 # print(rs)
 # demo.Graph(0, ('x', 'y'), [newD[:, [0, ]], predicted])
+
+# from Helper import DataPreprocessing
+#
+# demo = DataPreprocessing()
+# demo.readParagraph("sushiReview.txt", True, '\t')
+# dictionary, dataMat = demo.wordBagging(demo.ENGLISH, demo.ND_ARRAY, lambda x: len(x) > 3)
+# demo.DataSet = dataMat
+# demo.removeRedundantData()
